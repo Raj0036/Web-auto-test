89f0817265c24f9c303596b2e62a60f2b6a24b3d
Raj0036 <hdbtsujd@gmail.com>
2025-09-05 12:17:12 +0530

    Delete adaptive-attack-simulator-github-ready directory

--- adaptive-attack-simulator-github-ready/README_DEMO.txt
+++
@@ -1,4 +0,0 @@
-
-Demo usage example:
-python aas_cli.py https://juice-shop.herokuapp.com
-Open reports/ to view generated files.
--- adaptive-attack-simulator-github-ready/aas/plugins/base.py
+++
@@ -1,7 +0,0 @@
-
-class PluginBase:
-    def __init__(self, target, config=None):
-        self.target = target
-        self.config = config or {}
-    def run(self):
-        raise NotImplementedError
